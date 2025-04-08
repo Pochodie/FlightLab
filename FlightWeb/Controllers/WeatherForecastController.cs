bf49b2b0ee767638a755e26aedf0ec29d361df5d
Daniel Stagno <daniel@stagno.net>
2025-04-08 20:26:03 +0200

    Working??

--- FlightWeb/Controllers/WeatherForecastController.cs
+++ FlightWeb/Controllers/WeatherForecastController.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightWeb.Controllers
@@ -7,7 +6,6 @@ namespace FlightWeb.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -23,13 +21,10 @@ namespace FlightWeb.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-
-
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                //TemperatureC = Random.Shared.Next(-20, 55),
-                TemperatureC = AirplaneInfo.BigPlaneCount,
+                TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
--- FlightWeb/WeatherForecast.cs
+++ FlightWeb/WeatherForecast.cs
@@ -9,13 +9,5 @@ namespace FlightWeb
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-
-
-    }
-
-    public static class AirplaneInfo
-    {
-        public static int SmallPlaneCount { get; set; } = 22;
-        public static int BigPlaneCount { get; set; } = 52;
     }
 }
